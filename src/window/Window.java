a13370c01a90c07dc95e13e6e41f6fa11dbcf79d
Yusra64 <yusra.siyad@city.ac.uk>
2023-03-11 17:56:09 +0000

    refactored name charactercontroller to playercontroller

--- src/window/Window.java
+++ src/window/Window.java
@@ -1,14 +1,5 @@
 package window;
 
-import city.cs.engine.*;
-import city.cs.engine.Shape;
-import city.cs.engine.BoxShape;
-import org.jbox2d.common.Vec2;
-
-import java.awt.*;
-import java.awt.event.KeyListener;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
 import javax.swing.*;
 
 public class Window {
@@ -24,7 +15,7 @@ public class Window {
         GiveFocus focus = new GiveFocus(view);
         view.addMouseListener(focus);
 
-        CharacterController controller = new CharacterController(world.getCharacter());
+        PlayerController controller = new PlayerController(world.getCharacter());
         view.addKeyListener(controller);
         world.addStepListener(new Tracker(view, world.getCharacter()));
         final JFrame frame = new JFrame(" Best Pirate Game ARR-Bout");
